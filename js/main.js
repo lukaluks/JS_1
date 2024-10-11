699ee9230831b2f34e860319f6f6d8e07ba9d238
lukaluks <163586723+lukaluks@users.noreply.github.com>
2024-10-11 16:35:42 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let name = "Дмитро";
+let isStudent = true; 
+let myString = "Життя не шахмати, одного мата не вистачить"; 
+let myNumber = 42; 
+myNumber += 10;
+let myNull = null;
+let userName = prompt("Luka Herzig");
+alert("Привіт, " + userName + "!");
+console.log(myNull);
+console.log(myNumber);
+console.log(myString);
+console.log(isStudent);
+console.log(name);
